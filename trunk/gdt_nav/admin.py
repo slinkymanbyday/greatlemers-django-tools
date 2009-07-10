111eee9bd9078e8ee553eab0dace5c8ea00f2d6b
greatlemer <greatlemer@2b261dee-4401-11de-86e1-e9dff1ea4737>
2009-07-10 06:30:52 +0000

    Initial addition of custom admin pages for the creation of menu options.  See README for more information on how to use these pages.

--- trunk/gdt_nav/admin.py
+++ trunk/gdt_nav/admin.py
@@ -1,6 +1,8 @@
 from models import MenuGroup, MenuOption
 from django.contrib.admin import site, ModelAdmin, VERTICAL
 from django.contrib.sites.models import Site
+from forms import AbsoluteMenuOptionForm, ModelMenuOptionForm,\
+                  NamedMenuOptionForm
 site.register(MenuGroup)
 
 class MenuOptionAdmin(ModelAdmin):
@@ -58,3 +60,136 @@ class MenuOptionAdmin(ModelAdmin):
   )
 
 site.register(MenuOption, MenuOptionAdmin)
+class AbsoluteMenuOptionAdmin(ModelAdmin):
+  list_display = ('name', 'option_type', 'menu_group',)
+  search_fields = ('name',)
+  list_filter = ('show_to_anonymous',
+                 'show_to_authenticated',
+                 'show_to_staff',
+                )
+  ordering = ('menu_group', 'name',)
+  filter_horizontal = ('permissions',)
+  radio_fields = {'option_type':VERTICAL,}
+  form = AbsoluteMenuOptionForm
+  fieldsets = (
+    (None, {'fields': ('name',
+                       'alt_text',
+                       'url',
+                       'notes',
+                       'option_type',
+                      ),
+           },
+    ),
+    ('Positioning',{'fields':((Site._meta.installed and ('sites',
+                              'menu_group',
+                              'parent',
+                              'ordering',
+                             )) or ('menu_group',
+                              'parent',
+                              'ordering',
+                             )),
+                    'description':'Settings describing where to display the menu item.',
+                   },
+    ),
+    ('Permissions', {'fields': ('show_to_anonymous',
+                                'show_to_authenticated',
+                                'show_to_staff',
+                                'permissions',
+                               ),
+                     'description':'Settings to determine who can see the menu item.',
+                    },
+    ),
+  )
+
+class NamedMenuOptionAdmin(ModelAdmin):
+  list_display = ('name', 'option_type', 'menu_group',)
+  search_fields = ('name',)
+  list_filter = ('show_to_anonymous',
+                 'show_to_authenticated',
+                 'show_to_staff',
+                )
+  ordering = ('menu_group', 'name',)
+  filter_horizontal = ('permissions',)
+  radio_fields = {'option_type':VERTICAL,}
+  form = NamedMenuOptionForm
+  fieldsets = (
+    (None, {'fields': ('name',
+                       'alt_text',
+                       'url_name',
+                       'notes',
+                       'option_type',
+                      ),
+           },
+    ),
+    ('Positioning',{'fields':((Site._meta.installed and ('sites',
+                              'menu_group',
+                              'parent',
+                              'ordering',
+                             )) or ('menu_group',
+                              'parent',
+                              'ordering',
+                             )),
+                    'description':'Settings describing where to display the menu item.',
+                   },
+    ),
+    ('Permissions', {'fields': ('show_to_anonymous',
+                                'show_to_authenticated',
+                                'show_to_staff',
+                                'permissions',
+                               ),
+                     'description':'Settings to determine who can see the menu item.',
+                    },
+    ),
+  )
+
+class ModelMenuOptionAdmin(ModelAdmin):
+  list_display = ('name', 'option_type', 'menu_group',)
+  search_fields = ('name',)
+  list_filter = ('show_to_anonymous',
+                 'show_to_authenticated',
+                 'show_to_staff',
+                )
+  ordering = ('menu_group', 'name',)
+  filter_horizontal = ('permissions',)
+  radio_fields = {'option_type':VERTICAL,}
+  form = ModelMenuOptionForm
+  fieldsets = (
+    (None, {'fields': ('name',
+                       'alt_text',
+                       'url_name',
+                       'notes',
+                       'option_type',
+                      ),
+           },
+    ),
+    ('Positioning',{'fields':((Site._meta.installed and ('sites',
+                              'menu_group',
+                              'parent',
+                              'ordering',
+                             )) or ('menu_group',
+                              'parent',
+                              'ordering',
+                             )),
+                    'description':'Settings describing where to display the menu item.',
+                   },
+    ),
+    ('Permissions', {'fields': ('show_to_anonymous',
+                                'show_to_authenticated',
+                                'show_to_staff',
+                                'permissions',
+                               ),
+                     'description':'Settings to determine who can see the menu item.',
+                    },
+    ),
+    ('Model Information', {'fields': ('content_type',
+                                      'manager',
+                                      'query',
+                                      'url_id',
+                                      'model_id',
+                                      'order_by',
+                                      'result_limit',
+                                     ),
+                           'description':'Information describing the model and query that will select what should appear for this menu item.',
+                          },
+    ),
+  )
